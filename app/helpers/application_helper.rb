4b3b91c8a8ba1930f79d365d112dfd1ba05ac05e
Adelino Segundo <adelinosegundo@gmail.com>
2016-09-06 09:47:57 -0300

    fix to tag translator

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,9 @@
 module ApplicationHelper
   def translate_tags user_data, text
-    tags = text.scan(/&lt;&lt;([^<>]*)&gt;&gt;/imu).flatten
-    # tags = text.scan(/<<([^<>]*)>>/).flatten
-    tag_data = ""
+    tags = text.scan(/&lt;&lt;(.*?)&gt;&gt;/imu).flatten
+    # tags = text.scan(/<<(.*?)>>/).flatten
     tags.each do |tag|
-      tag_data = tag.split(".")
-      text["&lt;&lt;#{tag}&gt;&gt;"] = user_data[tag]
+      text = text.sub(/&lt;&lt;#{tag}&gt;&gt;/, user_data[tag])
       # text["<<#{tag}>>"] = user_data[tag]
     end
     text
