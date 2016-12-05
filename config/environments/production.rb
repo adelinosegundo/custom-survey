450ac45b6a44636e65d8fe5d7800fe007f42c711
Adelino Segundo <adelinosegundo@gmail.com>
2016-12-05 15:49:27 -0300

    added host to app

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = { :host => '104.236.97.231' }
 end
