8e83f5515af4b6d032e81c6c9e73082fe5c7ae7c
Adelino Segundo <adelinosegundo@gmail.com>
2016-12-05 16:35:32 -0300

    testing host option in mailer

--- app/mailers/suport_mailer.rb
+++ app/mailers/suport_mailer.rb
@@ -4,7 +4,17 @@ class SuportMailer < ApplicationMailer
   def deliver_survey_mail_message reply
   	@recipient = reply.recipient
   	recipient_data = reply.mail_message.survey.users_data[reply.recipient.email]
-    @content = translate_tags recipient_data, reply.mail_message.content.dup, new_reply_survey_url(reply.mail_message.survey, reply.link_hash)
+    @content = translate_tags(
+    	recipient_data, 
+    	reply.mail_message.content.dup, 
+    	url_for(
+	    	controller: 'surveys',
+	    	action: 'new_reply',
+	    	id: reply.mail_message.survey.id,
+	    	link_hash: reply.link_hash,
+	    	host: '104.236.97.231'
+		)
+	)
     reply.update(sended: true)
     mail(:to => reply.recipient.email, :subject => reply.mail_message.subject)
   end
