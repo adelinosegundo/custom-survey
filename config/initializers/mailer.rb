6f052844cc07d168f4d61ff2ade02e57953110ad
Adelino Segundo <adelinosegundo@gmail.com>
2016-12-08 13:50:59 -0300

    trying out smtp config

--- config/initializers/mailer.rb
+++ config/initializers/mailer.rb
@@ -1,7 +1,7 @@
 Rails.application.config.action_mailer.delivery_method = :smtp
 Rails.application.config.action_mailer.smtp_settings = {
   :authentication => :plain,
-  :address => "dimap.ufrn.br",
+  :address => "smtp.gmail.com",
   :port => 587,
   :domain => ENV['MAIL_DOMAIN'],
   :user_name => ENV['MAIL_USERNAME'],
