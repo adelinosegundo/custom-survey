872daecb78a6d2934faa93f7b2a724e36566e7f2
Adelino Segundo <adelinosegundo@gmail.com>
2017-10-20 12:20:26 -0300

    test fix mailer

--- config/initializers/mailer.rb
+++ config/initializers/mailer.rb
@@ -1,5 +1,6 @@
 Rails.application.config.action_mailer.smtp_settings = {
   :authentication => :plain,
+  :enable_starttls_auto => true,
   :address => "smtp.gmail.com",
   :port => 587,
   :domain => ENV['MAIL_DOMAIN'],
