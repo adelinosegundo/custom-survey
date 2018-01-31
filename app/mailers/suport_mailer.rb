9e991f7657deeca6cb5c708acf7a4858ca967b54
Adelino Segundo <adelinosegundo@gmail.com>
2018-01-31 17:35:54 -0300

    testing mail config

--- app/mailers/suport_mailer.rb
+++ app/mailers/suport_mailer.rb
@@ -14,6 +14,8 @@ class SuportMailer < ApplicationMailer
     subject = translate_tags recipient_data, mail_message.subject.dup
 
     recipient.update(sended: true)
-    mail(:to => recipient.email, :subject => subject, delivery_method: :smtp)
+    mail = mail(:to => recipient.email, :subject => subject, delivery_method: :smtp)
+    mail.delivery_method
+    mail.delivery_method.settings.merge!(Rails.application.secrets.gmail_smtp)
   end
 end
