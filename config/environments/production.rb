c18b699a6ef98840dbafe46d4f89d22e03ad5d4b
Adelino Segundo <adelinosegundo@gmail.com>
2020-02-11 15:54:24 -0300

    Add host/domain to env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,15 +76,15 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { :host => 'survey.dimap.ufrn.br' }
-  config.action_controller.default_url_options = { host: 'survey.dimap.ufrn.br' }
+  config.action_mailer.default_url_options = { :host => ENV['HOST'] }
+  config.action_controller.default_url_options = { host: ENV['HOST'] }
 
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :mailgun
   config.action_mailer.mailgun_settings = {
     api_key: ENV['MAILGUN_API_KEY'],
-    domain: 'survey.dimap.ufrn.br'
+    domain: ENV['MAILGUN_DOMAIN']
   }
 end
 
-Rails.application.routes.default_url_options[:host] = 'survey.dimap.ufrn.br'
+Rails.application.routes.default_url_options[:host] = ENV['HOST']
